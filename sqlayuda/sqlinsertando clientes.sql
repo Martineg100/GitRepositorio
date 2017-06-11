e36bfb95519b90dc2a061ddb384e3b3881e0e580
Martineg100 <User@DESKTOP-7EI8CJE>
2017-06-10 23:18:41 -0300

    2017-06-10 luego de actualizar testtaller

---
+++ sqlayuda/sqlinsertando clientes.sql
@@ -0,0 +1,19 @@
+
+
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Gonzalez','Martin Emanuel',32)
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Mamani','Hugo',31)
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Guaraz','Natalia',33)
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Gonzalez','Claudia',31)
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Campos','Lidia',54)
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Apellido1','Nombre1',44)
+INSERT INTO [dbo].[Cliente] ([Apellido],[Nombre],[Edad])
+     VALUES ('Gomez','Papu',55)
+
+
+
